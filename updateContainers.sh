5ba5f5042b6ad252024fa56f40aa76cd59ca4530
stat1c <fynn.weyrich@web.de>
2020-10-16 15:41:23 +0200

    added update script

--- updateContainers.sh
+++ updateContainers.sh
@@ -1,4 +1,6 @@
-./stopContainers.sh
+docker-compose down
+docker system prune -f
+docker rmi $(docker images -a -q)
 cd nosql-quiz-frontend
 git pull
 cd ~
@@ -7,4 +9,8 @@ git pull
 cd ~
 cd nosql-quiz-hosting
 git pull
+/bin/cp -a . ..
 cd ~
+chmod 600 acme.json
+docker network create web
+docker-compose up
